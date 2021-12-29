8cae883f933b8cd6b59dc94d9b1733aeedfe8940
Alexey Stemasov <alekseistemasov@gmail.com>
2021-12-30 01:22:34 +0300

    changes

--- MoreStore/app/src/main/java/com/project/morestore/fragments/Registration4Fragment.kt
+++ MoreStore/app/src/main/java/com/project/morestore/fragments/Registration4Fragment.kt
@@ -1,13 +1,26 @@
 package com.project.morestore.fragments
 
+import android.icu.text.DisplayContext
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.project.morestore.R
 import com.project.morestore.databinding.FragmentRegistration1Binding
+import ru.tinkoff.decoro.MaskImpl
+import ru.tinkoff.decoro.slots.PredefinedSlots
+import ru.tinkoff.decoro.watchers.MaskFormatWatcher
+import java.lang.reflect.Type
+import java.sql.Types
+import ru.tinkoff.decoro.parser.UnderscoreDigitSlotsParser
+import androidx.annotation.NonNull
+import com.redmadrobot.inputmask.MaskedTextChangedListener
+
 
 class Registration4Fragment : Fragment(R.layout.fragment_registration1) {
     private val binding: FragmentRegistration1Binding by viewBinding()
@@ -17,12 +30,38 @@ class Registration4Fragment : Fragment(R.layout.fragment_registration1) {
         super.onViewCreated(view, savedInstanceState)
         setClickListeners()
         initToolbar()
-        binding.registerTextView.text =
+
             if (!args.phoneOrEmail.contains(Regex("[a-z]")) || args.phoneOrEmail.isEmpty()) {
-                "Почта"
+                binding.registerTextView.text = "Почта"
+                binding.phoneEmailEditText.inputType = android.text.InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+                //val slots = ("___@___")
+                //val formatWatcher = MaskFormatWatcher(MaskImpl.createNonTerminated(slots))
+                //formatWatcher.installOn(binding.phoneEmailEditText)
+
+                //val listener = MaskedTextChangedListener("[…]@[AAA]", binding.phoneEmailEditText)
+                //binding.phoneEmailEditText.addTextChangedListener(listener)
+                //binding.phoneEmailEditText.onFocusChangeListener = listener
+
+
+
+
             } else {
-                "Телефон"
+                binding.registerTextView.text = "Телефон"
+                binding.phoneEmailEditText.inputType = android.text.InputType.TYPE_CLASS_PHONE
+                val listener = MaskedTextChangedListener("+7([000])-[000]-[00]-[00]", binding.phoneEmailEditText)
+                binding.phoneEmailEditText.addTextChangedListener(listener)
+                //val mask = MaskImpl.createTerminated(PredefinedSlots.RUS_PHONE_NUMBER);
+                //val watcher = MaskFormatWatcher(mask)
+                //watcher.installOn(binding.phoneEmailEditText)
             }
+
+        //binding.phoneEmailEditText.setOnFocusChangeListener { view, b ->
+           // binding.phoneEmailEditText.setMask("+7(###)-###-##-##")
+           // Log.d("mylog", "onFocusChange")
+
+        //}
+
+
     }
 
     private fun setClickListeners() {
