60db8e6dd92777df0de5056d3c9373eeb4006375
Evgenix93 <estemasov6@gmail.com>
2022-03-08 01:07:33 +0300

    changes

--- MoreStore/app/src/main/java/com/project/morestore/presenters/MainPresenter.kt
+++ MoreStore/app/src/main/java/com/project/morestore/presenters/MainPresenter.kt
@@ -314,8 +314,7 @@ class MainPresenter(context: Context) : MvpPresenter<MainMvpView>() {
 
   fun loadFilter(){
       presenterScope.launch{
-        val isLoaded =  userRepository.loadFilter()
-          if(isLoaded)
+          userRepository.loadFilter()
           viewState.loaded(userRepository.getFilter())
       }
   }
