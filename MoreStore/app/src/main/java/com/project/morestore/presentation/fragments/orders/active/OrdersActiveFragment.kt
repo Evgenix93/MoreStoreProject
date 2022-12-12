c05db1c7c10e4a7db62c64b43fa04f7de5103fe9
user <alekseistemasov@gmail.com>
2022-12-12 15:01:11 +0300

    update

--- MoreStore/app/src/main/java/com/project/morestore/presentation/fragments/orders/active/OrdersActiveFragment.kt
+++ MoreStore/app/src/main/java/com/project/morestore/presentation/fragments/orders/active/OrdersActiveFragment.kt
@@ -55,7 +55,7 @@ class OrdersActiveFragment
         toolBarCartPresenter.initMenu(OrdersSliderMenu.ORDERS)
         showBottomNav()
         initToolbar()
-        showLoading(true)
+
     }
 
     ///////////////////////////////////////////////////////////////////////////
