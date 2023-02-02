ba5451fc96b9ab07bb2b7470e5a0293bbf3d554d
user <alekseistemasov@gmail.com>
2023-02-02 17:19:45 +0300

    update

--- MoreStore/app/src/main/java/com/project/morestore/data/models/PaymentUrl.kt
+++ MoreStore/app/src/main/java/com/project/morestore/data/models/PaymentUrl.kt
@@ -1,8 +1,10 @@
 package com.project.morestore.data.models
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class PaymentUrl(
+    @Json(name = "payment_url")
     val formUrl: String
 )
