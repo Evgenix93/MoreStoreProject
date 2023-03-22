8b04b147e6d754af8c8329e0544f6bc53c2c78e1
user <alekseistemasov@gmail.com>
2023-03-22 15:34:15 +0300

    update

---
+++ MoreStore/app/src/main/java/com/project/morestore/domain/presenters/CabinetPresenter.kt
@@ -0,0 +1,7 @@
+package com.project.morestore.domain.presenters
+
+import com.project.morestore.presentation.mvpviews.CabinetMvpView
+import moxy.MvpPresenter
+
+class CabinetPresenter: MvpPresenter<CabinetMvpView>() {
+}
---
+++ MoreStore/app/src/main/java/com/project/morestore/presentation/mvpviews/CabinetMvpView.kt
@@ -0,0 +1,6 @@
+package com.project.morestore.presentation.mvpviews
+
+import moxy.MvpView
+
+interface CabinetMvpView: MvpView {
+}
